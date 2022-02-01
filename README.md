13563deb96715689c768d3818569360d6a682c3b
Nikan <52320631+nikanmog@users.noreply.github.com>
2022-02-01 12:06:40 +0100

    Recordings are persisted with pandas dataframes and feather

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 * Tested with Python 3.10, PyCharm is recommended as an IDE
 * Install requirements with pip (venv recommended)
 
-Run this in the project repository (where the requirements.txt is located) with your terminal (global python installation required)
+Run this in the project repository (where the requirements.txt is located) with your terminal (global python
+installation required)
 
 ```
 pip install -r requirements.txt
--- helpers/data_storage.py
+++ helpers/data_storage.py
@@ -2,14 +2,15 @@ from datetime import datetime
 
 import pandas as pd
 
-dataframe = pd.DataFrame()
+recording_buffer = list()
 
 
 def add_recording(data: tuple):
-    dataframe.append(data)
+    recording_buffer.append(data)
 
 
 def persist_recordings():
+    dataframe = pd.DataFrame(data=recording_buffer, columns=[str(x) for x in range(82)])
     timestamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     print(dataframe.head(10))
-    dataframe.to_feather("/data/" + timestamp)
+    dataframe.to_feather("data/" + timestamp)
