eda70d791de2320aa91f8a90259b2a2cce7d9cfd
Nikan <52320631+nikanmog@users.noreply.github.com>
2022-02-01 12:52:34 +0100

    Updated byte message that is sent to syncstation+

--- helpers/data_storage.py
+++ helpers/data_storage.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 import pandas as pd
 
+from helpers.environment_variables import CHANNEL
+
 recording_buffer = list()
 
 
@@ -10,7 +12,7 @@ def add_recording(data: tuple):
 
 
 def persist_recordings():
-    dataframe = pd.DataFrame(data=recording_buffer, columns=[str(x) for x in range(82)])
+    dataframe = pd.DataFrame(data=recording_buffer, columns=[str(x) for x in range(CHANNEL)])
     timestamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     print(dataframe.head(10))
     dataframe.to_feather("data/" + timestamp)
